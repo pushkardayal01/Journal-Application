413dee3608670b01937189ce364b082583431fc2
Pushkar <pushkardayal07@gmail.com>
2024-08-31 16:51:55 +0530

    journal Application setup

---
+++ src/main/java/com/pushkar/journalapplication/JournalapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.pushkar.journalapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pushkar/journalapplication/JournalapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pushkar.journalapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
